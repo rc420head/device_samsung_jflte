7a270fc038b755e3bef18b8defb03df005e94134
nicklovell23 <nicklovell23@gmail.com>
2014-09-25 06:25:38 +0200

    Remove common_multicarrier
    Change-Id: I810213c66740377f7adbfc9c9c342eecfaed4805

--- slim.mk
+++ slim.mk
@@ -6,8 +6,5 @@ $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 # Inherit some common slim stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-# Inherit multicarrier.
-$(call inherit-product, vendor/slim/config/common_multicarrier.mk)
-
 PRODUCT_NAME := slim_jflte
 PRODUCT_DEVICE := jflte
